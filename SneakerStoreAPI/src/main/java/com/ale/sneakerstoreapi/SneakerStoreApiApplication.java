d9b6afd5d49bae8c83ebe1c41e1f7cc86d393097
Linh <71812422+AlE-dip@users.noreply.github.com>
2023-07-15 15:01:04 +0700

    create project

---
+++ SneakerStoreAPI/src/main/java/com/ale/sneakerstoreapi/SneakerStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.ale.sneakerstoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakerStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SneakerStoreApiApplication.class, args);
+	}
+
+}
